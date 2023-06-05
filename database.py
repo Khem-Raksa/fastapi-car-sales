296bcae2452423dd2898db1554edc7fd5599b88a
Khem Raksa Peou <kraksaprofessional@gmail.com>
2023-06-05 09:16:40 +0700

    fastapi carsale

---
+++ database.py
@@ -0,0 +1,47 @@
+cars ={
+1:{
+    "make" : "CarPro",
+    "model" : "Supersonic",
+    "year" : 2015,
+    "price" : 235000,
+    "engine" : "V12",
+    "autonomous" : False,
+    "sold" : ["EU"]
+}, 
+2:{
+    "make" : "Toyota",
+    "model" : "Supersonic",
+    "year" : 2013,
+    "price" : 235000,
+    "engine" : "V12",
+    "autonomous" : False,
+    "sold" : ["DC"]
+},
+3:{
+    "make" : "Ford",
+    "model" : "Supersonic",
+    "year" : 2010,
+    "price" : 235000,
+    "engine" : "V12",
+    "autonomous" : False,
+    "sold" : ["OC"]
+},
+4:{
+    "make" : "Bugatti",
+    "model" : "Supersonic",
+    "year" : 2011,
+    "price" : 235000,
+    "engine" : "V12",
+    "autonomous" : False,
+    "sold" : ["FA"]
+},
+5:{
+    "make" : "Mercedes",
+    "model" : "Supersonic",
+    "year" : 2009,
+    "price" : 235000,
+    "engine" : "V12",
+    "autonomous" : False,
+    "sold" : ["NA"]
+}
+}
